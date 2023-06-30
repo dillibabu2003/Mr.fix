bd26bba33001653c66ee26e144f1acc732a1a92e
ASVIN <asvin.vt2021csea@sece.ac.in>
2023-06-30 14:50:20 +0530

    Initial commit

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+export default function Profile(){
+    const [data, setData] = useState([]);
 
-const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+    useEffect(() => {
+      fetchData();
+    }, []);
+  
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+        setData(response.data);
+        console.log("printing got values",data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    
+  useEffect(() => {
+    console.log("Printing fetched data:", data);
+  }, [data]);
+
+    return (
+         <><div>
+            <h2>Welcome to Profile Page</h2>
+            {JSON.stringify(data)}
+         </div>
+
+        </>
+    )
 }
-
-export default Profile
