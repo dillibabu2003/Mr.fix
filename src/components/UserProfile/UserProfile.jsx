ed575078ef4ce1472ce04bc7a281064d275b6ac6
dillibabu2003 <dillibabu.r2021csea@sece.ac.in>
2023-07-11 23:50:16 +0530

    user design changed

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -3,56 +3,20 @@ import proimg from "./noprofil.jpg";
 
 function UserProfile() {
   return (
-    <div>
-      <h1>Profile</h1>
-      <div className="profile-container">
-        <img
-          src={proimg}
-          alt="profile_pic"
-          className="profile-container-proimg"
-        />
-      </div>
-      <br />
-      <div className="edit">
-        <button className="edit-button">Edit</button>
-      </div>
-      <div className="details">
-        <br />
-        <div className="user-id">
-          <h3>User Id</h3>
-          <div className="id">
-            <h4>5445</h4>
+    <div className="wrapper">
+      <div className="profile-wrapper">
+        <div className="image-and-id">
+          <img className="profile-img" src={proimg} />
+          <div className="userid-and-name">
+            <div className="user-name"> Dilli Babu</div>
+            <div className="user-id"> <b>Id: </b>&nbsp; seceuser001 </div>
           </div>
         </div>
-        <br />
-        <div className="user-name">
-          <h3>User Name</h3>
-          <div className="name">
-            <h4>jaya</h4>
-          </div>
-        </div>
-        <br />
-        <div className="e-mail">
-          <h3>E-mail</h3>
-          <div className="mail">
-            <h4>jayasurya123@gmail.com</h4>
-          </div>
-        </div>
-        <br />
-        <div className="password">
-          <h3>Password</h3>
-          <div className="pass">
-            <h4>sece#321</h4>
-          </div>
-        </div>
-        <br />
-        <div className="phone-no">
-          <h3>Phone No</h3>
-          <div className="number">
-            <h4>9360340528</h4>
-          </div>
-        </div>
-        <br />
+        <div className="info">
+        <div className="info-items"> <b>Email &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;:</b> dillisurya@gmail.com</div>
+        <div className="info-items"><b>PassWord  &nbsp; &nbsp;:</b> ds@1234</div>
+        <div className="info-items"><b>Contact &nbsp; &nbsp; &nbsp; &nbsp;:</b>9360324636</div>
+       </div>
       </div>
     </div>
   );
