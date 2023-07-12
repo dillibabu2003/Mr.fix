714c72d63c777bc3ba4be160f4f3eef82ae45721
dillibabu2003 <dillibabu.r2021csea@sece.ac.in>
2023-07-12 23:18:32 +0530

    manage added

--- src/components/Manage/Manage.jsx
+++ src/components/Manage/Manage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+
+import "./Manage.css";
+import { Table } from "@mui/material";
 
 const Manage = () => {
   return (
-    <div>Manage</div>
-  )
-}
+    <div>
+      <br />
+      <h1>Manage</h1>
+      <div className="container">
+        <div className="manage-events">
+          <div className="manage-events-button">View All Complaints</div>
+        </div>
+        <div className="box">
+          <input type="text" placeholder="Search here..."></input>
+          <a href="#">
+            <i class="fas fa-search"></i>
+          </a>
+        </div>
+      </div>
+      <Table />
+    </div>
+  );
+};
 
-export default Manage
+export default Manage;
